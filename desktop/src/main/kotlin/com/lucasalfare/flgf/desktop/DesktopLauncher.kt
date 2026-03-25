0625dd9699ee280ab5e7a0e735297475623efafe
LucasAlfare <god-lucas@hotmail.com>
2026-03-25 01:50:56 -0300

    update: include a simple skeleton visualization

--- desktop/src/main/kotlin/com/lucasalfare/flgf/desktop/DesktopLauncher.kt
+++ desktop/src/main/kotlin/com/lucasalfare/flgf/desktop/DesktopLauncher.kt
@@ -2,12 +2,12 @@ package com.lucasalfare.flgf.desktop
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
-import com.lucasalfare.flgf.core.MyGame
+import com.lucasalfare.flgf.core.GuitarFlashGame
 
 fun main() {
   val config = Lwjgl3ApplicationConfiguration()
   config.setTitle("Meu Jogo")
   config.setWindowedMode(800, 600)
 
-  Lwjgl3Application(MyGame(), config)
+  Lwjgl3Application(GuitarFlashGame(), config)
 }
